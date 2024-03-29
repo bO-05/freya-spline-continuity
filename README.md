2781c56b85035c3a501e18b759db40209c246621
G. A. 02 <blessedbyhope05@gmail.com>
2024-03-29 11:15:30 +0700

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## The Continuity of Splines: A Deep Dive into Curve Smoothness
+
+Auto-generated documentation site from [The Continuity of Splines)](https://www.youtube.com/watch?v=jvPPXbo87ds).
+
+Created using [Lumentis](https://github.com/hrishioa/lumentis)
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>The Continuity of Splines: A Deep Dive into Curve Smoothness - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="The Continuity of Splines: A Deep Dive into Curve Smoothness" />
+          <meta property="og:description" content="This video explores the concept of splines, a fundamental tool for creating smooth paths in various fields such as game development, computer graphics, and industrial design. It delves into different types of splines, their properties, and the mathematical concepts behind them, with a focus on continuity and its impact on the smoothness of the resulting curves." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
